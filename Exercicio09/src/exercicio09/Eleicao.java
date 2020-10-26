f1ca47d090c980b42339b5827e46f7a9d3e865bf
Jeferson Amorim Leite <jamoriml@everis.com>
2020-10-26 09:37:30 -0300

    Alteração na lógica

--- Exercicio09/src/exercicio09/Eleicao.java
+++ Exercicio09/src/exercicio09/Eleicao.java
@@ -2,40 +2,23 @@ package exercicio09;
 import java.util.Scanner;
 
 public class Eleicao {
+	
+	public double TotalEleitores;
+	public double Validos;
+	public double Brancos;
+	public double Nulos;
+	
+	
+	public double PercentValidos() {
+		return (Validos / TotalEleitores) * 100;
+	}
+	
+	public double PercentBrancos() {
+		return (Brancos / TotalEleitores) * 100;
+	}
+	
+	public double PercentNulos() {
+		return (Nulos / TotalEleitores) * 100;
+	}
 
-	public int totaleleitores;
-	public int validos;
-	public int brancos;
-	public int nulos;
-	public int totalvalidos;
-	public int totalnulos;
-	public int totalbrancos;
-	
-	public void obterDados() {
-		System.out.println("Digite o total de eleitores");
-		totaleleitores = new Scanner(System.in).nextInt();
-		
-		System.out.println("Digite o total de votos validos");
-		validos = new Scanner(System.in).nextInt();
-		
-		System.out.println("Digite o total de votos nulos");
-		nulos = new Scanner(System.in).nextInt();
-		
-		System.out.println("Digite o total de votos brancos");
-		brancos = new Scanner(System.in).nextInt();
-	}
-	
-	public void percentual() {
-		
-		totalvalidos = (validos / totaleleitores) * 100;
-		
-		totalnulos = (nulos / totaleleitores) * 100;
-		
-		totalbrancos = (brancos / totaleleitores) * 100;
-		
-		
-		System.out.println("total de votos validos" +validos + "%");
-		System.out.println("total de votos nulos" +nulos + "%");
-		System.out.println("total de votos brancos" +brancos + "%");
-	}
 }
--- Exercicio09/src/exercicio09/Main.java
+++ Exercicio09/src/exercicio09/Main.java
@@ -3,12 +3,22 @@ package exercicio09;
 public class Main {
 
 	public static void main(String[] args) {
-		Eleicao votos = new Eleicao();
 		
-		votos.obterDados();
+		Eleicao eleicao = new Eleicao();
+		eleicao.TotalEleitores = 200;
+		eleicao.Validos = 10;
+		eleicao.Brancos = 40;
+		eleicao.Nulos = 150;
 		
-		votos.percentual();
 		
+		double percentValidos = eleicao.PercentValidos();
+		System.out.println("Percentual de votos válidos: " + percentValidos);
+		
+		double percentBrancos = eleicao.PercentBrancos();
+		System.out.println("Percentual de votos brancos: " + percentBrancos);
+		
+		double percentNulos = eleicao.PercentNulos();
+		System.out.println("Percentual de votos nulos: " + percentNulos);
 		
 
 	}
